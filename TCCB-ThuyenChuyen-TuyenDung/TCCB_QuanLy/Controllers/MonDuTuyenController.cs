589abcacc278c1f729f055d847e7d29d8e815eed
truonghuuthanh <truonghuuthanh95@gmail.com>
2018-08-18 10:45:04 +0700

    init

---
+++ TCCB-ThuyenChuyen-TuyenDung/TCCB_QuanLy/Controllers/MonDuTuyenController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TCCB_QuanLy.Controllers
+{
+    [RoutePrefix("tuyendung")]
+    public class MonDuTuyenController : Controller
+    {
+        // GET: MonDuTuyen
+        [Route("mondutuyen")]
+        [HttpGet]
+        public ActionResult MonDuTuyen()
+        {
+            return View();
+        }
+        [Route("mondutuyen/capnhat")]
+        [HttpGet]
+        public ActionResult CapNhatMonDuTuyen()
+        {
+            return View();
+        }
+    }
+}
